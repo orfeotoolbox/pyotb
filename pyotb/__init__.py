ea7e4770196797a298c49cab04486ee9ada449ab
Vincent Delbar <vincent.delbar@latelescop.fr>
2022-02-16 16:31:04 +0000

    Improve user experience

--- pyotb/__init__.py
+++ pyotb/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "1.0.2"
-
-from .core import App, Output, Input, get_nbchannels, get_pixel_type
+# -*- coding: utf-8 -*-
+__version__ = "1.1"
 
 from .apps import *
-
+from .core import App, Output, Input, get_nbchannels, get_pixel_type
 from .functions import *
+from .tools import logger
