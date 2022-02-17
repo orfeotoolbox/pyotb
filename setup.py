54504f96b463ff6853821894def463ccd082e160
Narcon Nicolas <nicolas.narcon@inrae.fr>
2022-02-17 12:41:54 +0100

    ENH: enable python>=3.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyotb",
-    version="1.2",
+    version="1.2.2",
     author="Nicolas Narçon",
     author_email="nicolas.narcon@gmail.com",
     description="Library to enable easy use of the Orfeo Tool Box (OTB) in Python",
@@ -16,13 +16,17 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: GIS",
         "Topic :: Scientific/Engineering :: Image Processing",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    python_requires="==3.5.*",
+    python_requires=">=3.5",
     keywords="remote sensing, otb, orfeotoolbox, orfeo toolbox",
 )
 #package_dir={"": "src"},
