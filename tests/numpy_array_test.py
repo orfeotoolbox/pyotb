2615654899f51cbecbd7772fb2f030962d47483a
Vincent Delbar <vincent.delbar@latelescop.fr>
2022-07-18 17:52:09 +0200

    CI: print WKT to debug test

--- tests/numpy_array_test.py
+++ tests/numpy_array_test.py
@@ -41,4 +41,6 @@ crs = osr.SpatialReference()
 crs.ImportFromEPSG(2154)
 dest_crs = osr.SpatialReference()
 dest_crs.ImportFromWkt(profile['crs'])
-assert crs.ExportToWkt() == dest_crs.ExportToWkt()
+wkt1, wkt2 = crs.ExportToWkt(), dest_crs.ExportToWkt()
+print(f"{wkt1=}, {wkt2=}")
+assert wkt1 == wkt2
