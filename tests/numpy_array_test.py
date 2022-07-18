ad6c8d22595ba112cb5db919f32a6be6996fbc73
Vincent Delbar <vincent.delbar@latelescop.fr>
2022-07-18 14:11:27 +0200

    CI: add test for numpy arrays ops and to_rasterio

--- tests/numpy_array_test.py
+++ tests/numpy_array_test.py
@@ -2,17 +2,40 @@ import numpy as np
 import pyotb
 
 filepath = 'Data/Input/QB_MUL_ROI_1000_100.tif'
-inp = pyotb.Input(filepath)
 
+# Test to_numpy array
+inp = pyotb.Input(filepath)
+array = inp.to_numpy()
+assert array.dtype == np.float64
+assert array.shape == (100, 100, 4)
+
+# Test to_numpy array with slicer
+inp = pyotb.Input(filepath)[:50, :60, :3]
+array = inp.to_numpy()
+assert array.dtype == np.float64
+assert array.shape == (50, 60, 3)
+
+# Test conversion to numpy array
+array = np.array(inp)
+assert isinstance(array, np.ndarray)
+assert inp.shape == array.shape
+
+# Test image + noise README example
+white_noise = np.random.normal(0, 50, size=inp.shape)
+noisy_image = inp + white_noise
+assert isinstance(noisy_image, pyotb.App)
+assert noisy_image.shape == inp.shape
+
+# Check to_rasterio
 array, profile = inp.to_rasterio()
 
 # Check data type and shape
-assert array.dtype == np.float64
-assert array.shape == (4, 100, 100)
+assert array.dtype == profile['dtype'] == np.float64
+assert array.shape == (3, 50, 60)
 
 # Check array statistics
-assert array.min() == 126.0
-assert array.max() == 1973.0
+assert array.min() == 136.0
+assert array.max() == 591.0
 
 # Check rasterio profile
 assert profile['transform'] == (1.0, 0.0, 1000.0, 0.0, 1.0, 1000.0)
