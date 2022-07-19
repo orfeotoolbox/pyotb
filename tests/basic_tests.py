39922ce26777dd22b8ffd3be844b9b609a3db224
Vincent Delbar <vincent.delbar@latelescop.fr>
2022-07-19 11:49:37 +0200

    CI: fix test with new App's argument "quiet"

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -10,16 +10,16 @@ extract = inp[:50, :60, :3]
 assert extract.shape == (50, 60, 3)
 
 # Test ReadImageInfo
-info = pyotb.ReadImageInfo(inp, otb_stdout=False)
+info = pyotb.ReadImageInfo(inp, quiet=True)
 assert info.sizex == 251
 assert info.sizey == 304
 assert info['numberbands'] == info.numberbands == 4
 
 # Test Statistics
-stats = pyotb.ComputeImagesStatistics(il=inp, otb_stdout=False)
+stats = pyotb.ComputeImagesStatistics(il=inp, quiet=True)
 assert stats['out.min'] == "[33, 64, 91, 47]"
 
 # Test Statistics on a Slicer
-slicer_stats = pyotb.ComputeImagesStatistics(il=[inp[:10, :10, 0]], otb_stdout=False)
+slicer_stats = pyotb.ComputeImagesStatistics(il=[inp[:10, :10, 0]], quiet=True)
 assert slicer_stats['out.min'] == '[180]'
 
