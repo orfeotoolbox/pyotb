481fe5c595822215a21fd43636c6284fb5105d4f
Vincent Delbar <vincent.delbar@latelescop.fr>
2022-09-19 19:25:02 +0200

    ENH: accurate dict indent in serialization test

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -10,14 +10,14 @@ def test_pipeline_simple():
     app2 = pyotb.OrthoRectification({'io.in': app1})
     app3 = pyotb.ManageNoData({'in': app2})
     summary = app3.summarize()
-    reference = {'name': 'ManageNoData',
-                'parameters': {'in': {'name': 'OrthoRectification',
-                'parameters': {'io.in': {'name': 'BandMath',
-                    'parameters': {'il': ('tests/image.tif',), 'exp': 'im1b1'}},
+    reference = {'name': 'ManageNoData', 'parameters': {'in': {
+                    'name': 'OrthoRectification', 'parameters': {'io.in': {
+                        'name': 'BandMath', 'parameters': {'il': ('tests/image.tif',), 'exp': 'im1b1'}},
                     'map': 'utm',
                     'outputs.isotropic': True}},
                 'mode': 'buildmask'}}
     assert summary == reference
+
 
 def test_pipeline_diamond():
     # Diamond graph
@@ -26,18 +26,17 @@ def test_pipeline_diamond():
     app3 = pyotb.ManageNoData({'in': app2})
     app4 = pyotb.BandMathX({'il': [app2, app3], 'exp': 'im1+im2'})
     summary = app4.summarize()
-    reference = {'name': 'BandMathX',
-                'parameters': {'il': [{'name': 'OrthoRectification',
-                    'parameters': {'io.in': {'name': 'BandMath',
-                    'parameters': {'il': ('tests/image.tif',), 'exp': 'im1b1'}},
+    reference = {'name': 'BandMathX', 'parameters': {'il': [
+                    {'name': 'OrthoRectification', 'parameters': {'io.in': {
+                        'name': 'BandMath', 'parameters': {'il': ('tests/image.tif',), 'exp': 'im1b1'}},
                     'map': 'utm',
                     'outputs.isotropic': True}},
-                {'name': 'ManageNoData',
-                    'parameters': {'in': {'name': 'OrthoRectification',
-                    'parameters': {'io.in': {'name': 'BandMath',
-                        'parameters': {'il': ('tests/image.tif',), 'exp': 'im1b1'}},
-                    'map': 'utm',
-                    'outputs.isotropic': True}},
-                    'mode': 'buildmask'}}],
+                    {'name': 'ManageNoData', 'parameters': {'in': {
+                        'name': 'OrthoRectification', 'parameters': {
+                            'io.in': {'name': 'BandMath', 'parameters': {'il': ('tests/image.tif',), 'exp': 'im1b1'}},
+                            'map': 'utm',
+                            'outputs.isotropic': True}},
+                    'mode': 'buildmask'}}
+                ],
                 'exp': 'im1+im2'}}
     assert summary == reference
