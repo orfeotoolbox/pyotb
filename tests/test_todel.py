6eb1c98276357bbd25f3cc60bb8d2e649298605e
Remi Cresson <remi.cresson@inrae.fr>
2023-01-25 16:12:13 +0100

    DOC: code and tests

--- tests/test_todel.py
+++
@@ -1,6 +0,0 @@
-import pyotb
-
-img_pth = "/vsicurl/https://gitlab.orfeo-toolbox.org/orfeotoolbox/" \
-          "otb/-/raw/develop/Data/Input/SP67_FR_subset_1.tif?inline=false"
-bm = pyotb.MeanShiftSmoothing({"in": img_pth, "fout": "/tmp/toto.tif", "foutpos": "/tmp/titi.tif"})
-print(bm.find_outputs())
