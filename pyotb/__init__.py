afc8290e9eac6e8c0adc3655e5912f1608c79638
Vincent Delbar <vincent.delbar@latelescop.fr>
2023-06-01 21:25:39 +0000

    Resolve "CI trigger is not right"

--- pyotb/__init__.py
+++ pyotb/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """This module provides convenient python wrapping of otbApplications."""
-__version__ = "2.0.0.dev1"
+__version__ = "2.0.0.dev2"
 
 from .helpers import logger, set_logger_level
 from .apps import *
