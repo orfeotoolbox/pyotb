36681d77311ec6cd38d4d1a5a85347a7d7cc2a38
Vincent Delbar <vincent.delbar@latelescop.fr>
2023-08-10 21:34:15 +0200

    CI: bump version

--- pyotb/__init__.py
+++ pyotb/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """This module provides convenient python wrapping of otbApplications."""
-__version__ = "2.0.0.dev4"
+__version__ = "2.0.0.dev5"
 
 from .helpers import logger, set_logger_level
 from .core import (
